ea0e26a35a4b59acd5421c133d078d805fea266f
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-07-15 00:07:11 +0100

    feat: App layout built

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,68 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
+
+const GlobalStyles = createGlobalStyle`
+  /* @font-face {
+    font-family: 'radnika_next';
+    src: url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+    font-weight: normal;
+    font-style: normal;
+  } */
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height:2;
+    min-height: 100vh;
+  }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  footer {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    padding: 20px;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
@@ -14,28 +71,14 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
+    <GlobalStyles />
+    <Header />
+    <InnerStyles>{children}</InnerStyles>
+    <hr />
     <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+      <span>© {new Date().getFullYear()} Space X</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
