5888b72effb0e90f8ba6ebb9ae6f88c9c2eff855
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-07-15 00:05:51 +0100

    chore: Added progress bar on route changes and typed the expected components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
+import { FunctionComponent } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
 import { ApolloProvider } from '@apollo/client';
+import NProgress from 'nprogress';
 import { useApollo } from '../lib/apolloClient';
+import '../components/styles/nprogress.css';
 
 import 'normalize.css/normalize.css';
+import Layout from '../components/Layout';
+
+// Show progress bar when page is loading.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const TypedComponent = Component as FunctionComponent;
   const apolloClient = useApollo(pageProps);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <Layout title="Home | Next.js + TypeScript + Graphql">
+        <TypedComponent {...pageProps} />
+      </Layout>
     </ApolloProvider>
   );
 };
